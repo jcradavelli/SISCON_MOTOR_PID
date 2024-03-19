e69ac7663527a74dc81e28bd983b9b19ac6ed2a8
jcradavelli <43486489+jcradavelli@users.noreply.github.com>
2024-03-19 13:48:19 -0300

    dividida aplicação em tasks

--- main/rotary_encoder_example_main.c
+++ main/rotary_encoder_example_main.c
@@ -56,8 +56,146 @@ void stop_mottor (encmot_h encmot)
 }
                 
 
+typedef struct tskEncmot_args_
+{
+    encmot_h encmot;
+    QueueHandle_t logQueue;
+}tskEncmot_args_t;
+void tsk_encmot (void *args)
+{
+    tskEncmot_args_t *this;
+    encmotDebugStream_t encmotStream;
+    this = args;
+    assert (args != NULL);
+    assert (this->logQueue != NULL);
+
+    while(true)
+    {
+        vTaskDelay(pdMS_TO_TICKS(100));
+        encmot_job (this->encmot, &encmotStream);
+
+        if (xQueueSend(this->logQueue, &encmotStream, 0) != pdPASS)
+        {
+            ESP_LOGE(TAG, "LogQueue FULL!");
+        }
+    }
+}
+
+typedef struct tskGraph_args_
+{
+    QueueHandle_t logQueue;
+}tskGraph_args_t;
+void tsk_graph (void *args)
+{
+    tskGraph_args_t *this;
+    encmotDebugStream_t received;
+    this = args;
+
+    assert (args != NULL);
+    assert (this->logQueue != NULL);
+
+    while(true)
+    {
+        xQueueReceive(this->logQueue, &received, portMAX_DELAY);
+
+        printf(
+            "\n---------- PID controller job ----------------\n"
+            ">measurement:\t\t%e\n"
+            ">setpoint:\t%e\n"
+            // ">error:\t\t%e\n"
+            // ">kp: %e\n"
+            // ">ki: %e\n"
+            // ">kd: %e\n"
+            // ">proportional:\t%e\n"
+            // ">integrator:\t\t%e\n"
+            // ">differentiator:\t\t%e\n"
+            ">out:\t\t%d\n"
+            "\n----------------------------------------------\n",
+            received.PID.measurement,
+            received.PID.setpoint,
+            // received.PID.error,
+            // received.PID.Kp,
+            // received.PID.Ki,
+            // received.PID.Kd,
+            // received.PID.proportional,
+            // received.PID.integrator,
+            // received.PID.differentiator,
+            received.PID.out
+        );
+    }
+}
+
+typedef struct tskInput_args_
+{
+    encmot_h encmot;
+}tskInput_args_t;
 float kp=5000.0, ki=15000.0, kd = 50.0;
 float* selected = &kp;
+void tsk_input (void *args)
+{
+    tskInput_args_t *this;
+    assert (args != NULL);
+    this = args;
+
+    bool butonsClear = false;
+
+    while (1)
+    {
+
+        vTaskDelay(pdMS_TO_TICKS(100));
+
+        if (butonsClear == true)
+        {
+
+            if (!gpio_get_level(BUTTON_1))
+            {
+                ESP_LOGD(TAG,"CONTINUE/SELECT");
+                togle_setpoint (this->encmot);
+                //encmot_continue(encmot);
+                butonsClear = false;
+            }
+
+            if (!gpio_get_level(BUTTON_2))
+            {
+                // incrementa o ganho
+                *selected += 0.1;
+                butonsClear = false;
+            }
+
+            if (!gpio_get_level(BUTTON_3))
+            {
+                // decrementa o ganho
+                *selected -= 0.1;
+                butonsClear = false;
+            }
+
+            if (!gpio_get_level(BUTTON_4))
+            {
+                ESP_LOGD(TAG,"STOP");
+                stop_mottor(this->encmot);
+
+                if (selected == &kp) selected = &ki;
+                else if (selected == &ki) selected = &kd;
+                else if (selected == &kd) selected = &kp;
+                butonsClear = false;
+            }
+
+        }
+        else if (gpio_get_level(BUTTON_1) && gpio_get_level(BUTTON_2) && gpio_get_level(BUTTON_3) && gpio_get_level(BUTTON_4))
+        {
+            butonsClear = true;
+            encmot_tune_pid(this->encmot,kp,ki,kd); // TODO: Ajustar PID
+            ESP_LOGD(TAG,"GANHOS-------------------\n>kp:%e\n>ki:%e\n>kd:%e\n------------------",kp,ki,kd);
+
+        }
+    }
+}
+
+
+
+tskInput_args_t tskInputArgs;
+tskGraph_args_t tskGraphArgs;
+tskEncmot_args_t tskEncmotArgs;
 void app_main(void)
 {
 
@@ -108,58 +246,19 @@ void app_main(void)
     };
     ESP_ERROR_CHECK(gpio_config(&gpio_config_led));
 
-    while (1) {
-       
-        vTaskDelay(pdTICKS_TO_MS(1));
 
-        encmot_job (encmot, NULL);
-        
-        gpio_set_level(LED_OUT,satured);
+    QueueHandle_t LogQueue = xQueueCreate(10, sizeof(encmotDebugStream_t));
+    
+    tskInputArgs.encmot = encmot;
+    xTaskCreatePinnedToCore(tsk_input, "input", /* Stack Size = */ 2048 , &tskInputArgs, /* Priority= */configMAX_PRIORITIES/2, NULL, 1);
 
+    tskGraphArgs.logQueue = LogQueue;
+    xTaskCreatePinnedToCore(tsk_graph, "graph", /* Stack Size = */ 2048 , &tskGraphArgs, /* Priority= */configMAX_PRIORITIES/2-1, NULL, 0);
 
-        if (butonsClear == true)
-        {
+    tskEncmotArgs.encmot = encmot;
+    tskEncmotArgs.logQueue = LogQueue;
+    xTaskCreatePinnedToCore(tsk_encmot, "input", /* Stack Size = */ 2048 , &tskEncmotArgs, /* Priority= */configMAX_PRIORITIES/2+1, NULL, 1);
 
-            if (!gpio_get_level(BUTTON_1))
-            {
-                ESP_LOGD(TAG,"CONTINUE/SELECT");
-                togle_setpoint (encmot);
-                //encmot_continue(encmot);
-                butonsClear = false;
-            }
+    esp_log_level_set("encoder", ESP_LOG_NONE);
 
-            if (!gpio_get_level(BUTTON_2))
-            {
-                // incrementa o ganho
-                *selected += 0.1;
-                butonsClear = false;
-            }
-
-            if (!gpio_get_level(BUTTON_3))
-            {
-                // decrementa o ganho
-                *selected -= 0.1;
-                butonsClear = false;
-            }
-
-            if (!gpio_get_level(BUTTON_4))
-            {
-                ESP_LOGD(TAG,"STOP");
-                stop_mottor(encmot);
-
-                if (selected == &kp) selected = &ki;
-                else if (selected == &ki) selected = &kd;
-                else if (selected == &kd) selected = &kp;
-                butonsClear = false;
-            }
-
-        }
-        else if (gpio_get_level(BUTTON_1) && gpio_get_level(BUTTON_2) && gpio_get_level(BUTTON_3) && gpio_get_level(BUTTON_4))
-        {
-            butonsClear = true;
-            encmot_tune_pid(encmot,kp,ki,kd); // TODO: Ajustar PID
-            ESP_LOGD(TAG,"GANHOS-------------------\n>kp:%e\n>ki:%e\n>kd:%e\n------------------",kp,ki,kd);
-
-        }
-    }
 }
